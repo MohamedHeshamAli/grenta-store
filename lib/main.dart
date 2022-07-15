9531aec2289ed153338902f69dc9066a18e36eb5
Mohamed Hesham Ali <54541528+MohamedHeshamAli@users.noreply.github.com>
2022-07-15 16:50:15 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget kdshfjkhfsdkh
-ff
-  sdjfh
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
